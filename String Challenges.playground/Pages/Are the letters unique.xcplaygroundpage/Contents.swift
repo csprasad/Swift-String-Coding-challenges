2ddf3efd3a82a7ee5638382f330b8d15ca993501
CSPrasad <csprasad195@gmail.com>
2022-06-24 22:03:41 +0530

    New String chanllenge added "Run Length Encoding"

--- String Challenges.playground/Pages/Are the letters unique.xcplaygroundpage/Contents.swift
+++ String Challenges.playground/Pages/Are the letters unique.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 func duplicate(input: String) -> Bool {
 
@@ -15,8 +16,13 @@ func duplicate(input: String) -> Bool {
     // return true
     
     //This method will perform faster bcz Set dont contain dubplicate values
+    let array = Array(input)
+    let sortedArray = Array(NSOrderedSet(array:array))
+    print(sortedArray)
+    
     return Set(input).count == input.count
 }
 
 
 print(duplicate(input: "abcdefghijklmnopqrstuvwxyz"))
+//print(duplicate(input: "122334455"))
